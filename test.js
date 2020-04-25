3d56f374dfc7ad0d42a0ac689c460527621803ca
Ting Ting Ye <tingtingye24@gmail.com>
2020-04-25 17:54:08 -0400

    added csv reader

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 fs.createReadStream('data.csv')
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row);
+    console.log(row); //read in csv anad console.log
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
